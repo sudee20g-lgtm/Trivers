f6095df324a47f7ec73f3cf428d9031b9700befd
sudee20g-lgtm <sudee20g@gmail.com>
2025-12-04 01:22:27 +0300

    Buzul

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "900c3aede4df7cb6f9108a199ee88b93
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "176e6400a269564cb1eb3cfed5dc6ef4",
+"flutter_bootstrap.js": "61374dec8bb25a4e4114f0882dc3fdf4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
